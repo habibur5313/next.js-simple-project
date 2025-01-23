5afbbfbeb8c1bc8602af2577db15dfee3cdbacea
habibur5313 <habiburmamun313@gmail.com>
2025-01-23 16:35:30 +0600

    eslient updated

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,3 +1,23 @@
+// import { dirname } from "path";
+// import { fileURLToPath } from "url";
+// import { FlatCompat } from "@eslint/eslintrc";
+
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = dirname(__filename);
+
+// const compat = new FlatCompat({
+//   baseDirectory: __dirname,
+//   resolvePluginsRelativeTo: __dirname, // Ensure relative path for plugins
+// });
+
+// const eslintConfig = [
+//   ...compat.extends("next/core-web-vitals"),
+//   // Add any additional rules or plugins here
+// ];
+
+// export default eslintConfig;
+
+
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
@@ -7,8 +27,12 @@ const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
+  resolvePluginsRelativeTo: __dirname, // Ensure relative path for plugins
 });
 
-const eslintConfig = [...compat.extends("next/core-web-vitals")];
+const eslintConfig = [
+  ...compat.extends("next/core-web-vitals"),
+  // Add any additional rules or plugins heresdf
+];
 
 export default eslintConfig;
